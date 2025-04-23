ec64b74cad261ac9179b9a56bc88b39178fec210
Anu-Bhav <myster.anubhav@gmail.com>
2025-04-23 10:09:57 +0000

    Apr 23, 2025, 2:09 PM

--- hello.py
+++ hello.py
@@ -7,4 +7,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print(torch.__version__)
+    print(torch.backends.mps.is_available())
+    print(torch.backends.mps.is_built())
     print(torch.cuda.is_available())
