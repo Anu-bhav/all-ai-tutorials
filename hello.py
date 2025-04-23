4cdff7eafecde6f8301babfbb9d990862fa0bf7b
Anu-Bhav <myster.anubhav@gmail.com>
2025-04-23 09:54:00 +0000

    Apr 23, 2025, 1:54 PM

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
+import torch
+
+
 def main():
     print("Hello from all-ai-tutorials!")
 
 
-import torch
-
-print(torch.cuda.is_available())
-
 if __name__ == "__main__":
+    print(torch.cuda.is_available())
     main()
